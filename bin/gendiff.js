ca4e1364e088369944a8641b3cb591e0a54ae7db
Bosqy <groo@vth.ru>
2023-01-31 09:54:49 +1000

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+import { program } from 'commander';
+
+const command = () => {
+  console.log('It Works!');
+}
+
+program
+  .version('0.0.1')
+  .action(command)
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format', 'output format')
+  .parse(process.argv);
 

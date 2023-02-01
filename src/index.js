79112f321495776a945204f0d88c382555b6d269
Bosqy <groo@vth.ru>
2023-02-01 20:24:13 +1000

    genDiff is better

--- src/index.js
+++ src/index.js
@@ -7,22 +7,35 @@ const fileToJSON = (file) => {
 };
 
 const genDiff = (...files) => {
-  const [file1, file2] = files.map(fileToJSON).map(Object.entries)
-    .map((json, index) => json.map((pair) => {
-        const first = index === 0 ? '-' : '+';
-        return _.concat(first, pair)}));
-    const result = _.concat(file1, file2);
-    const result2 =  result.map((rec) => {
-        if ( result.filter((innerRec) => (rec[1] === innerRec[1] && rec[2] === innerRec[2])).length === 2) 
-        {return [' ', rec[1], rec[2]]}
-        return rec;
-
-      })
-    const result3 = _.sortBy(result2, [function(a) {return a[1]}])
-     .map((innerRec) => `  ${innerRec[0]} ${innerRec[1]}: ${innerRec[2]}`)
-    console.log(_.uniq(result3).join('\n'));
+  const [file1, file2] = files
+        .map(fileToJSON)
+        .map(Object.entries)
+        .map((json) => json.map((line) => line.join(':')))
+    const inBoth = _.intersection(file1, file2)
+    const inFirst = _.difference(file1, file2)
+    const inSecond = _.difference(file2, file1)
+    const diff = _.flatten(
+      [
+        [' ', ...inBoth],
+        ['-', ...inFirst],
+        ['+', ...inSecond],
+      ]
+    .map((group) => {
+      const [first, ...rest] = group;
+      return rest.reduce((acc, line) => {
+        const [key, value] = line.split(':')
+        acc.push([first, key, value])
+          return acc;
+      }, [])
+    })
+    )
+    const diffFormatted = _.sortBy(diff, [function(a) {return a[1]}])
+      .map((innerRec) => `  ${innerRec[0]} ${innerRec[1]}: ${innerRec[2]}`)
+      .join('\n');
+//    console.log( `{\n${diffFormatted}\n}`);
+    return `{\n${diffFormatted}\n}`;
 };
-
 export default genDiff;
 
-genDiff('/home/groo/frontend-project-46/fixtures/file1.json', '/home/groo/frontend-project-46/fixtures/file2.json');
+//  genDiff('/home/groo/frontend-project-46/fixtures/file1.json', '/home/groo/frontend-project-46/fixtures/file2.json');
+

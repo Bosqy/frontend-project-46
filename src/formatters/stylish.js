dbac21c68bf6be103479711996da2afa02a3778f
Bosqy <groo@vth.ru>
2023-02-22 20:57:36 +1000

    fix stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,47 +1,43 @@
 import _ from 'lodash';
-import genDiffTree from '../tree.js';
 
-const getStylishLabel = (status) => {
-  switch (status) {
-    case 'updated':
-      return '+-';
-    case 'unchanged':
-    case 'tree':
-      return '  ';
-    case 'added':
-      return '+ ';
-    case 'removed':
-      return '- ';
-    default:
-      throw new Error(`Unknown status: ${status}`);
+const getIndent = (depth) => {
+  const replacer = '  ';
+  return depth > 0 ? `${replacer.repeat(depth)}` : '';
+};
+
+const stringify = (value, depth) => {
+  if (_.isPlainObject(value)) {
+    const indent = getIndent(depth * 2 + 2);
+    const entries = Object.entries(value)
+      .map(([entryKey, entryValue]) => `${indent}${entryKey}: ${stringify(entryValue, depth + 1)}`);
+    const closingBrace = `${getIndent(depth * 2)}}`;
+    return ['{', ...entries, closingBrace].join('\n');
   }
+  return value;
 };
 
 const getStylish = (tree) => {
-  const replacer = '  ';
   const iter = (innerTree, depth) => {
     const content = innerTree.map((el) => {
-      const stylishLabel = getStylishLabel(el.status);
-      const indent = `${replacer.repeat(depth * 2 - 1)}${stylishLabel}`;
-      if (el.status === 'updated') {
-        const indentRemoved = indent.slice(0, -2).concat(getStylishLabel('removed'));
-        const indentAdded = indent.slice(0, -2).concat(getStylishLabel('added'));
-        const value = _.isPlainObject(el.value)
-          ? iter(genDiffTree(el.value, el.value), depth + 1)
-          : el.value;
-        const oldValue = _.isPlainObject(el.oldValue)
-          ? iter(genDiffTree(el.oldValue, el.oldValue), depth + 1)
-          : el.oldValue;
-        return `${indentRemoved}${el.key}: ${oldValue}\n${indentAdded}${el.key}: ${value}`;
+      const indent = `${getIndent(depth * 2 - 1)}`;
+      const value = stringify(el.value, depth);
+      const oldValue = stringify(el.oldValue, depth);
+      switch (el.status) {
+        case 'tree':
+          return `${indent}  ${el.key}: ${iter(el.children, depth + 1)}`;
+        case 'updated':
+          return `${indent}- ${el.key}: ${oldValue}\n${indent}+ ${el.key}: ${value}`;
+        case 'unchanged':
+          return `${indent}  ${el.key}: ${value}`;
+        case 'added':
+          return `${indent}+ ${el.key}: ${value}`;
+        case 'removed':
+          return `${indent}- ${el.key}: ${value}`;
+        default:
+          throw new Error(`Unknown status: ${el.status}`);
       }
-      if (_.has(el, 'children')) {
-        return `${indent}${el.key}: ${iter(el.children, depth + 1)}`;
-      }
-      return _.isPlainObject(el.value)
-        ? `${indent}${el.key}: ${iter(genDiffTree(el.value, el.value), depth + 1)}`
-        : `${indent}${el.key}: ${el.value}`;
     });
-    const closingBrace = `${replacer.repeat(depth * 2 - 2)}}`;
+    const closingBrace = `${getIndent(2 * depth - 2)}}`;
     return ['{', ...content, closingBrace].join('\n');
   };
   return iter(tree, 1);

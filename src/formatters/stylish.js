966d966f11031ff39a2e8fc489664ab85b9618e7
Bosqy <groo@vth.ru>
2023-02-26 14:49:00 +1000

    fix stylsih.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,26 +2,26 @@ import _ from 'lodash';
 
 const getIndent = (depth) => {
   const replacer = '  ';
-  return depth > 0 ? `${replacer.repeat(depth)}` : '';
+  return depth === 0 ? '' : `${replacer.repeat(depth * 2 - 1)}`;
 };
 
 const stringify = (value, depth) => {
+  const indent = getIndent(depth);
   if (_.isPlainObject(value)) {
-    const indent = getIndent(depth * 2 + 2);
     const entries = Object.entries(value)
-      .map(([entryKey, entryValue]) => `${indent}${entryKey}: ${stringify(entryValue, depth + 1)}`);
-    const closingBrace = `${getIndent(depth * 2)}}`;
+      .map(([entryKey, entryValue]) => `${indent}  ${entryKey}: ${stringify(entryValue, depth + 1)}`);
+    const closingBrace = `  ${getIndent(depth - 1)}}`;
     return ['{', ...entries, closingBrace].join('\n');
   }
-  return value;
+  return `${value}`;
 };
 
 const getStylish = (tree) => {
   const iter = (innerTree, depth) => {
     const content = innerTree.map((el) => {
-      const indent = `${getIndent(depth * 2 - 1)}`;
-      const value = stringify(el.value, depth);
-      const oldValue = stringify(el.oldValue, depth);
+      const indent = `${getIndent(depth)}`;
+      const value = stringify(el.value, depth + 1);
+      const oldValue = stringify(el.oldValue, depth + 1);
       switch (el.status) {
         case 'tree':
           return `${indent}  ${el.key}: ${iter(el.children, depth + 1)}`;
@@ -37,7 +37,7 @@ const getStylish = (tree) => {
           throw new Error(`Unknown status: ${el.status}`);
       }
     });
-    const closingBrace = `${getIndent(depth * 2 - 2)}}`;
+    const closingBrace = depth === 1 ? '}' : `  ${getIndent(depth - 1)}}`;
     return ['{', ...content, closingBrace].join('\n');
   };
   return iter(tree, 1);

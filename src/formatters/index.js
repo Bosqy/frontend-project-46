7eeadc8d26fcddfbdf38954a3a3f892401e9f25e
Bosqy <groo@vth.ru>
2023-02-28 09:00:20 +1000

    fix formatters/index.js (if to switch)

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,16 +2,16 @@ import getStylish from './stylish.js';
 import getPlain from './plain.js';
 
 const formatter = (tree, format) => {
-  if (format === 'stylish') {
-    return getStylish(tree);
+  switch (format) {
+    case 'stylish':
+      return getStylish(tree);
+    case 'plain':
+      return getPlain(tree);
+    case 'json':
+      return JSON.stringify(tree, null, '  ');
+    default:
+      throw new Error(`Unknown format: ${format}`);
   }
-  if (format === 'plain') {
-    return getPlain(tree);
-  }
-  if (format === 'json') {
-    return JSON.stringify(tree, null, '  ');
-  }
-  throw new Error(`Unknown format: ${format}`);
 };
 
 export default formatter;

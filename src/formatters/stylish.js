831d32af635a666fb604d6c8c2fa333efb092eb3
Bosqy <groo@vth.ru>
2023-03-06 15:49:46 +1000

    fix stylish.js (remove duplications)

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,8 @@
 import _ from 'lodash';
 
-const getIndent = (depth, label = 'blank') => {
+const getIndent = (depth) => {
   const indentPerDepth = 4;
-  const spaces = ' '.repeat(depth * indentPerDepth);
-
-  switch (label) {
-    case 'plus':
-      return `${spaces.slice(0, -2)}+ `;
-    case 'minus':
-      return `${spaces.slice(0, -2)}- `;
-    default:
-      return spaces;
-  }
+  return ' '.repeat(depth * indentPerDepth).slice(0, -2);
 };
 
 const stringify = (value, depth) => {
@@ -20,30 +11,31 @@ const stringify = (value, depth) => {
     return `${value}`;
   }
   const entries = Object.entries(value)
-    .map(([entryKey, entryValue]) => `${indent}${entryKey}: ${stringify(entryValue, depth + 1)}`);
-  const closingBrace = `${getIndent(depth - 1)}}`;
+    .map(([entryKey, entryValue]) => `${indent}  ${entryKey}: ${stringify(entryValue, depth + 1)}`);
+  const closingBrace = `${getIndent(depth - 1)}  }`;
   return ['{', ...entries, closingBrace].join('\n');
 };
 
 const getStylish = (tree) => {
   const iter = (innerTree, depth) => {
     const content = innerTree.map((el) => {
+      const indent = getIndent(depth);
       switch (el.status) {
         case 'nested':
-          return `${getIndent(depth)}${el.key}: ${iter(el.children, depth + 1)}`;
+          return `${indent}  ${el.key}: ${iter(el.children, depth + 1)}`;
         case 'changed':
-          return `${getIndent(depth, 'minus')}${el.key}: ${stringify(el.oldValue, depth + 1)}\n${getIndent(depth, 'plus')}${el.key}: ${stringify(el.value, depth + 1)}`;
+          return `${indent}- ${el.key}: ${stringify(el.oldValue, depth + 1)}\n${indent}+ ${el.key}: ${stringify(el.value, depth + 1)}`;
         case 'unchanged':
-          return `${getIndent(depth)}${el.key}: ${stringify(el.value, depth + 1)}`;
+          return `${indent}  ${el.key}: ${stringify(el.value, depth + 1)}`;
         case 'added':
-          return `${getIndent(depth, 'plus')}${el.key}: ${stringify(el.value, depth + 1)}`;
+          return `${indent}+ ${el.key}: ${stringify(el.value, depth + 1)}`;
         case 'deleted':
-          return `${getIndent(depth, 'minus')}${el.key}: ${stringify(el.value, depth + 1)}`;
+          return `${indent}- ${el.key}: ${stringify(el.value, depth + 1)}`;
         default:
           throw new Error(`Unknown status: ${el.status}`);
       }
     });
-    const closingBrace = depth === 1 ? '}' : `${getIndent(depth - 1)}}`;
+    const closingBrace = depth === 1 ? '}' : `${getIndent(depth - 1)}  }`;
     return ['{', ...content, closingBrace].join('\n');
   };
   return iter(tree, 1);
